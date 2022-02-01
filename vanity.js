455e45127ee8aa3985fb32c6af5d8b8195ff590a
James Bachini <james@mcambridge.com>
2022-02-01 14:54:55 +0000

    Option to search for multiple addresses

--- vanity.js
+++ vanity.js
@@ -1,18 +1,41 @@
 import { ethers } from 'ethers';
 
-const vanityAddress = '0xdef1'; // a-f 0-9
+//const vanityAddresses = ['0x111','0x222','0x333','0x444','0x555','0x666','0x777','0x888','0x999']; // a-f 0-9
+const vanityAddresses = ['0xdef1']; // a-f 0-9
 
-const vantiyLC = vanityAddress.toLowerCase();
+vanityAddresses.forEach((v,i) => { vanityAddresses[i] = v.toLowerCase() });
 
-const createAddress = () => {
+const findMultipleAddresses = async () => {
   const randomWallet = ethers.Wallet.createRandom();
-  if (randomWallet.address.toLowerCase().includes(vantiyLC)) {
-    console.log(randomWallet.address);
+  vanityAddresses.forEach((vanityAddress) => {
+    if (randomWallet.address.toLowerCase().includes(vanityAddress)) {
+      console.log(`# ${randomWallet.address}`);
+      console.log(randomWallet.address);
+      console.log(randomWallet._mnemonic().phrase);
+      console.log(randomWallet._signingKey().privateKey);
+      console.log('-------------------------------');
+    }
+  });
+}
+
+const findSingleAddress = async () => {
+  const randomWallet = ethers.Wallet.createRandom();
+  if (randomWallet.address.toLowerCase().includes(vanityAddresses[0])) {
+    console.log(`# ${randomWallet.address}`);
     console.log(randomWallet._mnemonic().phrase);
     console.log(randomWallet._signingKey().privateKey);
+    console.log('-------------------------------');
   }
 }
 
-for (let i = 0; i < 1e12; i++) {
-  createAddress();
+if (vanityAddresses.length > 1) {
+  console.log(`Searching for ${vanityAddresses.length} addresses`);
+  for (let i = 0; i < 1e12; i++) {
+    findMultipleAddresses();
+  }
+} else {
+  console.log(`Searching for single address ${vanityAddresses[0]}`);
+  for (let i = 0; i < 1e12; i++) {
+    findSingleAddress();
+  }
 }
